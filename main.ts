9ab676f1a67b1b5db7fed97b885253045c290512
ChrisAble2Shine <148521238+ChrisAble2Shine@users.noreply.github.com>
2024-02-19 17:25:03 +0800

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    hops_and_paws.vy = -160
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.setGameOverEffect(false, effects.melt)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hops_and_paws.vy == 0) {
+        hops_and_paws.vy = -190
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.setGameOverEffect(true, effects.confetti)
+})
+let coin: Sprite = null
 let hops_and_paws: Sprite = null
 scene.setBackgroundColor(9)
 hops_and_paws = sprites.create(img`
@@ -21,4 +37,27 @@ hops_and_paws = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(hops_and_paws, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
-hops_and_paws.ay = 200
+hops_and_paws.ay = 350
+scene.cameraFollowSprite(hops_and_paws)
+for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
+    coin = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.coin)
+    tiles.placeOnTile(coin, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
